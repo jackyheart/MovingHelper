cdbd8d151aeb0964a6112f41400cd6a5ea479b41
jackyheart <jacky.coolheart@gmail.com>
2015-09-24 17:22:54 +0800

    Starter project from raywenderlich.com

---
+++ MovingHelper/Extensions/FileLoadingStringExtension.swift
@@ -0,0 +1,33 @@
+//
+//  FileLoadingStringExtension.swift
+//  MovingHelper
+//
+//  Created by Ellen Shapiro on 6/15/15.
+//  Copyright (c) 2015 Razeware. All rights reserved.
+//
+
+import Foundation
+
+/*
+An extension to facilitate creating paths based on the receiving string.
+*/
+extension String {
+  
+  /*
+  Appends the receiver as a path component to the path of the NSDocumentDirectory.
+  
+  :returns: The full path to the receiver in the NSDocumentDirectory
+  */
+  func pathInDocumentsDirectory() -> String {
+    let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+    if let last = urls.last,
+      documentsDirectory = last.path {
+        
+        return last.URLByAppendingPathComponent(documentsDirectory).absoluteString
+        //return documentsDirectory.stringByAppendingPathComponent(self)
+    }
+    
+    //Fall-through
+    return self
+  }
+}
